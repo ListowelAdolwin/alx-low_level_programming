953bc2be3fbd4c4915cab04fa9f06f9e25badc83
ListowelAdolwin <listoweladolwin@gmail.com>
2022-07-29 23:20:02 +0000

    Add a description to the function

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -18,6 +18,10 @@ void free_list(hash_node_t *head)
 	}
 }
 
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table to be deleted
+ */
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
