50716ec59751dce1ea3ac71d7e59f7c30f4582dd
ListowelAdolwin <listoweladolwin@gmail.com>
2022-11-02 15:22:39 +0000

    updated header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -21,6 +21,7 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+skiplist_t *linear_skip(skiplist_t *list, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
